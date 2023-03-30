b6afb32b1fccb4acc218f126c9d640d3305c4c2e
Etec Profº Horácio Augusto da Silveira <rafaeltomaz2701@gmail.com>
2023-03-29 21:36:06 -0300

    fetLayout

---
+++ app/src/main/java/com/example/tiptime2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tiptime2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
